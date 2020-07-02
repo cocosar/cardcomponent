dd83416bc1d0c52faf9ae9237dc8c8fc28c27b7e
Juan José Sarsotti <cocosarsotti@gmail.com>
2020-07-01 21:07:15 -0300

    Boton mejorado y responsive

--- share.js
+++ share.js
@@ -1,14 +1,13 @@
+//Logica del boton Share
 let toggleShareStatus = false;
-
 const togglerShare = () => {
-    
-    let shareContainer = document.getElementById('share-container');
-       
-    if(toggleShareStatus == false){
-        shareContainer.style.visibility = 'visible';
-        toggleShareStatus = true;
-    } else {
-        shareContainer.style.visibility = 'hidden'
-        toggleShareStatus = false;
-    }
-}
+  let shareContainer = document.getElementById("share-container");
+
+  if (toggleShareStatus == false) {
+    shareContainer.style.visibility = "visible";
+    toggleShareStatus = true;
+  } else {
+    shareContainer.style.visibility = "hidden";
+    toggleShareStatus = false;
+  }
+};
